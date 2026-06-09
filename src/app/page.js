620ebeffc3b28c2022db293dace9da6440b39c2b
avishekroyyash <avishekroyyash@gmail.com>
2026-06-09 23:56:55 +0600

    setup jobportal frontend

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-7xl text-green-800 font-bold text-center">This is job home page </h1>
+    </div>
+  );
+}
